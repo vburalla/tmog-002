01065ff2303c600d661ea8c73ae3fd172347e9a8
Víctor Buralla Blasco <vburalla@mercadona.es>
2024-02-06 11:59:49 +0100

    fix: vbb solution

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -5,9 +5,25 @@ package org.tfoc;
  */
 public class Solution {
 
-    public boolean isValid(String s) {
+    private static final String OPENING_CHARS = "{[(";
+    private static final String CLOSING_CHARS = "}])";
 
-        return false;
+    public static boolean isValid(String s) {
+
+        String remaining = "";
+        int charIndex = 0;
+        for(int i = 0; i < s.length(); i++) {
+
+            char c = s.charAt(i);
+            if(OPENING_CHARS.indexOf(c) >= 0) {
+                remaining+=c;
+            } else if((charIndex = CLOSING_CHARS.indexOf(c)) >= 0 && remaining.length() > 0 && OPENING_CHARS.indexOf(remaining.charAt(remaining.length()-1)) == charIndex) {
+                remaining = remaining.substring(0, remaining.length()-1);
+            } else {
+                return false;
+            }
+        }
+        return remaining.isEmpty();
     }
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -2,6 +2,10 @@ package org.tfoc;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 class SolutionTest {
@@ -12,4 +16,11 @@ class SolutionTest {
         log.info("Don't forget to write the tests to make sure it works. Enjoy!");
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"(([]),false","({)}),false","(({}([]))),true", "([{[]}]]),false"})
+    void isValid(String input, boolean result) {
+
+        boolean response = Solution.isValid(input);
+        assertEquals(result, response);
+    }
 }
